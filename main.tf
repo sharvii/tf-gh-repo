5a96fdafe3d9b3bf1b717a3f78bff20dcfcd8c4c
snav <sharvigajanan.navelkar@ibm.com>
2025-07-16 11:40:49 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (us-east-1)
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
