032fbd64d92d07e6ff9f75e0b398f9cf3473a6d0
snav <sharvigajanan.navelkar@ibm.com>
2025-07-17 14:09:15 +0000

    Update tf yaml

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "sharvi-tf-state-bucket"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "sharvi-tf-state-bucket"
+
+#   tags = {
+#     Name        = "sharvi-tf-state-bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+#   bucket = aws_s3_bucket.example.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
+
+
+resource "aws_instance" "example" {
+
+ ami           = var.ami_id
+ instance_type = var.instance_type
 
   tags = {
-    Name        = "sharvi-tf-state-bucket"
-    Environment = "Dev"
-  }
+   Name = "Terraform-EC2"
+ }
 }
-
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-
-#resource "aws_instance" "example" {
-
- # ami           = var.ami_id
- # instance_type = var.instance_type
-#
-  #tags = {
-   # Name = "Terraform-EC2"
- # }
-#}
 
